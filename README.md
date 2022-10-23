86acf0c1a7f2b8336353b078b7ce49d9250fbc6e
Latifa Ajram <89570879+Latifa-Ajram@users.noreply.github.com>
2022-10-23 15:17:41 +0200

    Implementerte oppgaver 4,5,6 slik at de passet til alle testene.
    oppdatert readme fil ved å forklare hvordan jeg har løst disse oppgavene.

--- README.md
+++ README.md
@@ -20,3 +20,36 @@ I oppgave 3 startet jeg med å definere p sin forelder q, hvis den er null blir 
 Hvis høyre barn til q(forelder) er p eller er null, da er det q som skal returneres.
 ellers kaller vi metoden Postorden med q.høyere som parameter.
 
+I oppgave 4 
+Del 1: Iterativ postorden uten bruk av hjelpestakk. Jeg har først definert rot, så kalte jeg på
+metoden førstepostorden() for å hente den første noden i postorden traversering.
+og brukte en while løkke for å legge til nye noder ved kall på nestepostorden(). løkken foregår
+så lenge den første i postorden har en forelder
+
+Del 2: Jeg kaller postordenrekursive  første gang for å besøke venstre barn, basis tilfelle er når 
+venstre barn er lik null. så kaller jeg igjen metoden for å besøke høyre barn,basis tilfelle er når 
+høyre barn er null, til slutt besøker jeg noden (som er første verdi i postorden traversering)
+
+I oppgave 5 
+Del 1: Jeg startet med initialisering av et arraylist. og p som rotnode.
+deretter trengte jeg å initialisere en kø(jeg brukte type ArrayDeque).så lenge køen ikke er tom skjer dette:
+Roten legges inn i køen, så tas ut den første i køen, nodens venstre og høyre barn legges i køen, verdien til noden legges inn i arrayet.
+
+Del 2: jeg startet med å initialisere treet, så lopper jeg gjnnemon verdiene i arrayet ved bruk av
+for alle løkke , og legger hver eneste verdi i treet ved bruk av legg inn metoden.
+til slutt returneres treet.
+
+I oppgave 6
+Del 1 : fjern(T verdi), i tilfellene 1 og 2 ,skal pekeren q som er satt som forelder til p
+bli forelder til barnet til p etter at p fjernes.
+i tilfelle 3) så skal forelder(s) til noden som kommer nest  i innorden(r) etter noden vi vil fjerne  bli forlder til
+r.høyre. se kode.
+
+Del 2: fjernAlle(T verdi) jeg har startet med åinitialisere antlall verdier som skal fjernes.
+i en while løkke fjrener jeg verdiene ved kall på fjern metoden, antall økes for en hver fjerning som skjer, så returneres 
+antallet etter å ha fjernet alle veridier som tilsvarer input i metoden fjern(T verdi)
+
+Del 3: nullstill() her har jeg funnet først den første noden i postorden,ved bruk av metoden
+førstPostorden, og så lenge antall noder i treet ikke er lik 0 skjer dette:  bruk fjern(T verdi)metoden
+for å fjerne verdi, så settes den til null, og videre til neste node ved kall på nestePostorden. ved hver iterasjon reduseres antall noder.
+
--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -44,18 +44,18 @@ public class SBinTre<T> {
     }
 
     public boolean inneholder(T verdi) {
-        if (verdi == null) return false;
+        if (verdi == null) return false;// ingen null verdier
 
         Node<T> p = rot;
 
         while (p != null) {
-            int cmp = comp.compare(verdi, p.verdi);
-            if (cmp < 0) p = p.venstre;
-            else if (cmp > 0) p = p.høyre;
-            else return true;
+            int cmp = comp.compare(verdi, p.verdi);// sammenligner
+            if (cmp < 0) p = p.venstre; // går til venstre barn
+            else if (cmp > 0) p = p.høyre;// går til høyre barn
+            else return true; // verdien er funnet
         }
 
-        return false;
+        return false; // verdien ikke funnet
     }
 
     public int antall() {
@@ -118,11 +118,58 @@ public class SBinTre<T> {
 
 
     public boolean fjern(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if (verdi == null) return false;  // treet har ingen nullverdier
+
+        Node<T> p = rot, q = null;   // q skal være forelder til p
+
+        while (p != null)            // leter etter verdi
+        {
+            int cmp = comp.compare(verdi,p.verdi);      // sammenligner
+            if (cmp < 0) { q = p; p = p.venstre; }      // går til venstre
+            else if (cmp > 0) { q = p; p = p.høyre; }   // går til høyre
+            else break;    // den søkte verdien ligger i p
+        }
+        if (p == null) return false;   // finner ikke verdi
+
+        if (p.venstre == null || p.høyre == null)  // Tilfelle 1)bladnode og 2)p har nøyaktig ett barn
+        {
+            Node<T> b = p.venstre != null ? p.venstre : p.høyre;  // b for barn
+            if (p == rot) rot = b;
+            else if (p == q.venstre) q.venstre = b;
+            else q.høyre = b;
+            if(b != null)    b.forelder = q;// Nå får pekeren forelder korrekt verdi
+
+        }
+        else  // Tilfelle 3) p har to barn
+        {
+            Node<T> s = p, r = p.høyre;   // finner neste i inorden
+            while (r.venstre != null)
+            {
+                s = r;    // s er forelder til r
+                r = r.venstre;
+            }
+
+            p.verdi = r.verdi;   // kopierer verdien i r til p
+
+            if (s != p) s.venstre = r.høyre;
+            else s.høyre = r.høyre;
+            if(r.høyre != null) r.høyre.forelder = s;// Oppdaterer peker forelder
+        }
+
+        antall--;   // det er nå én node mindre i treet
+        endringer++;
+        return true;
     }
 
     public int fjernAlle(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+        int antall = 0;
+        while (fjern(verdi))antall++;
+        return antall;
+
     }
 
     public int antall(T verdi) {
@@ -147,7 +194,25 @@ public class SBinTre<T> {
 
 
     public void nullstill() {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+       // throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+
+        if(antall == 0)return;
+        Node<T> p= rot; // rot initialisering
+
+
+        p=førstePostorden(p);
+        while (antall != 0){
+
+            if(p != null) {
+                fjern(p.verdi);
+                p.verdi = null;
+                p=nestePostorden(p);
+            }
+
+            antall--;
+        }
+
     }
 
     private static <T> Node<T> førstePostorden(Node<T> p) {
@@ -167,15 +232,25 @@ public class SBinTre<T> {
 
         Node<T> q= p.forelder; // initialisering av p.forelder
 
-        if( q == null) return null;
+        if( q == null) return null;// hvis forelder er null så har vi kommet til siste node i treet(rot)
 
-        if(q.høyre == p || q.høyre== null) return q;
+        if(q.høyre == p || q.høyre== null) return q;// det er forelder som returneres hvis ingen høyre barn eller hvis hvis høyre barn er p
 
-        else return førstePostorden(q.høyre);
+        else return førstePostorden(q.høyre);// kaller på førstePostorden
     }
 
     public void postorden(Oppgave<? super T> oppgave) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+       // throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+        Node<T> p= rot;
+       Node<T> q =  førstePostorden(p);// dette er første node i postorden traversering
+        oppgave.utførOppgave(q.verdi);
+        while (q.forelder != null){ // så lenge den første noden har en forelder skal neste i postorden traversering hentes
+
+            q=nestePostorden(q);// kall på metoden for å hente neste.
+            oppgave.utførOppgave(q.verdi);
+
+        }
     }
 
     public void postordenRecursive(Oppgave<? super T> oppgave) {
@@ -183,15 +258,44 @@ public class SBinTre<T> {
     }
 
     private void postordenRecursive(Node<T> p, Oppgave<? super T> oppgave) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+       // throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+        if(p.venstre != null)postordenRecursive(p.venstre,oppgave);
+        if(p.høyre != null) postordenRecursive(p.høyre,oppgave);
+        oppgave.utførOppgave(p.verdi);
     }
 
     public ArrayList<T> serialize() {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+        ArrayList<T>a= new ArrayList<>();
+
+        Node<T> p = rot;
+        ArrayDeque<Node<T>> kø= new ArrayDeque<>();
+        kø.addLast(p);// rotnoden legges i køen
+        while (!kø.isEmpty()){
+
+            Node<T> q= kø.removeFirst();// første tas ut fra køen
+            if(q.venstre != null) kø.addLast(q.venstre);// legg til nodens venstre barn i køen
+            if(q.høyre != null)kø.addLast(q.høyre); // legg til nodens høyre barn i køen
+
+            a.add(q.verdi);// verdien legges i arrayet
+
+        }
+        return a;
+
     }
 
     static <K> SBinTre<K> deserialize(ArrayList<K> data, Comparator<? super K> c) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+      //  throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+     SBinTre<K> tre = new SBinTre<K>(c);// initialisering ev et binærtre
+
+     for (K verdi :data){ // går gjennom alle verdier i arraylist
+
+         tre.leggInn(verdi);// legger verdi i treet
+      }
+     return tre;
+
     }
 
 
